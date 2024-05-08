097784b93257ab7fee26748682fe37615a823f00
KingKennynas <josel.hernandezg@icloud.com>
2024-05-07 23:36:31 -0600

    flutter_serialcom

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'dart:typed_data';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:libserialport/libserialport.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +11,175 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Micromimic App',
+      home: MicromimicScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MicromimicScreen extends StatefulWidget {
+  const MicromimicScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MicromimicScreenState createState() => _MicromimicScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MicromimicScreenState extends State<MicromimicScreen> {
+  List<double> temperatureValues = [0]; // Initialize with a default value
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _setupSerialCommunication();
+  }
+
+  void _setupSerialCommunication() {
+    // Open the serial port
+    SerialPort port1 = SerialPort('COM3');
+    port1.openReadWrite();
+
+    // Set up a listener for incoming data
+    try {
+      SerialPortReader reader = SerialPortReader(port1);
+      Stream<double> upcommingData = reader.stream.map((data) {
+        // Assuming incoming data is a float represented as bytes
+        return double.parse(String.fromCharCodes(data));
+      });
+      upcommingData.listen((double data) {
+        setState(() {
+          temperatureValues.add(data);
+        });
+      });
+    } on SerialPortError catch (err, _) {
+      print(SerialPort.lastError);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Micromimic App'),
+        backgroundColor: Colors.green,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Medición de Temperatura',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Historical Values (Newest 5):',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 10),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: temperatureValues.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(temperatureValues[index].toString()),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(width: 20),
+            Flexible(
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: LineChart(
+                      LineChartData(
+                        gridData: FlGridData(show: false),
+                        titlesData: FlTitlesData(
+                          bottomTitles: SideTitles(
+                            showTitles: true,
+                            getTextStyles: (value) => const TextStyle(fontSize: 10, color: Colors.black),
+                            getTitles: (value) {
+                              // Customizing X axis titles (bottom)
+                              switch (value.toInt()) {
+                                case 0:
+                                  return '0';
+                                case 1:
+                                  return '1';
+                                case 2:
+                                  return '2';
+                                case 3:
+                                  return '3';
+                                case 4:
+                                  return '4';
+                                default:
+                                  return '';
+                              }
+                            },
+                          ),
+                          leftTitles: SideTitles(
+                            showTitles: true,
+                            getTextStyles: (value) => const TextStyle(fontSize: 10, color: Colors.black),
+                            getTitles: (value) {
+                              // Customizing Y axis titles (left)
+                              return value.toInt().toString();
+                            },
+                          ),
+                        ),
+                        borderData: FlBorderData(show: true, border: Border.all(color: Colors.black)),
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: temperatureValues.asMap().entries.map((entry) {
+                              return FlSpot(entry.key.toDouble(), entry.value);
+                            }).toList(),
+                            isCurved: true,
+                            colors: [Colors.blue],
+                            barWidth: 3,
+                            belowBarData: BarAreaData(show: false),
+                            dotData: FlDotData(show: false),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    _getTemperatureStatus(),
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  String _getTemperatureStatus() {
+    if (temperatureValues.isNotEmpty) {
+      double lastTemperature = temperatureValues.last;
+      if (lastTemperature < 35) {
+        return 'Hipotermia';
+      } else if (lastTemperature > 37) {
+        return 'Fiebre';
+      } else {
+        return 'Normal';
+      }
+    } else {
+      return 'No data';
+    }
   }
 }
